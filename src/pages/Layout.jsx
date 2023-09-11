9cb2fa8d1f4e82ec2926ce9cccb8793d955f1d12
njintechcom <services@njintech.com>
2023-09-11 22:38:13 +0700

    fix: stylePush

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -108,7 +108,7 @@ export default function Layout() {
         <img
           height="1"
           width="1"
-          style="display: none"
+          style={{ display: none }}
           src={`https://www.facebook.com/tr?id=${rootData.variables.pixel_id}&ev=PageView&noscript=1`}
         />
       </noscript>
